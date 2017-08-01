bb609fcfb177790228cf6a26a09307ddcbcd8a23
Yoann GALLAND <bahamut1221@gmail.com>
2017-08-01 19:34:52 +0200

    Finalize tuto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './services/in-memory-data.service';
+
 import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
@@ -11,6 +17,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HomeComponent } from './home/home.component';
 
 import { HeroService } from './services/hero.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +26,14 @@ import { HeroService } from './services/hero.service';
     FooterComponent,
     HeroDetailComponent,
     HeroesComponent,
-    HomeComponent
+    HomeComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [
