0d65dd161bc5c0586d261de8d880fff9d9e98f0d
Galand Yoann <yoann_galand@lm-c02jm0sbdv33.groupevsc.com>
2017-10-11 13:31:43 +0200

    Update macro ratio

--- client/app/macro/macro.component.ts
+++ client/app/macro/macro.component.ts
@@ -51,36 +51,94 @@ export class MacroComponent implements OnInit {
     let noxSeparator: number = 2;
 
     let positions = [
-      {
-        width: 0.6625,
-        height: 0.7639
-      },
-      {
-        width: 0.7617,
-        height: 0.7639
-      },
-      {
-        width: 0.8594,
-        height: 0.7639
-      },
-      {
-        width: 0.6625,
-        height: 0.2778
-      },
-      {
-        width: 0.7617,
-        height: 0.2778
-      },
-      {
-        width: 0.8594,
-        height: 0.2778
-      }
+      [
+        {
+          width: 0.6625,
+          height: 0.7639
+        },
+        {
+          width: 0.7617,
+          height: 0.7639
+        },
+        {
+          width: 0.8594,
+          height: 0.7639
+        },
+        {
+          width: 0.6625,
+          height: 0.2778
+        },
+        {
+          width: 0.7617,
+          height: 0.2778
+        },
+        {
+          width: 0.8594,
+          height: 0.2778
+        }
+      ],
+      [
+        {
+          width: 0.23,
+          height: 0.65
+        },
+        {
+          width: 0.23,
+          height: 0.75
+        },
+        {
+          width: 0.23,
+          height: 0.85
+        },
+        {
+          width: 0.7,
+          height: 0.65
+        },
+        {
+          width: 0.7,
+          height: 0.75
+        },
+        {
+          width: 0.7,
+          height: 0.85
+        }
+      ],
+      [
+        {
+          width: 0.3,
+          height: 0.6
+        },
+        {
+          width: 0.3,
+          height: 0.725
+        },
+        {
+          width: 0.3,
+          height: 0.85
+        },
+        {
+          width: 0.65,
+          height: 0.6
+        },
+        {
+          width: 0.65,
+          height: 0.725
+        },
+        {
+          width: 0.65,
+          height: 0.85
+        }
+      ]
     ];
 
-    this.memu += String(Math.round(memuMilliseconds + memuMilliseconds * frame)) + '--VINPUT--MULTI:1:0:' + Math.round(positions[position].width * this.width) + ':' + Math.round(positions[position].height * this.height) + '\n';
+    let positionRatio = this.width > this.height ? 0 : this.width < this.height ? 1 : 2;
+    let width = Math.round(positions[positionRatio][position].width * this.width)
+    let height = Math.round(positions[positionRatio][position].height * this.height)
+
+    this.memu += String(Math.round(memuMilliseconds + memuMilliseconds * frame)) + '--VINPUT--MULTI:1:0:' + width + ':' + height + '\n';
     this.memu += String(Math.round(memuMilliseconds + memuMilliseconds * frame + memuSeparator)) + '--VINPUT--MULTI:1:1:0:' + String(this.height) + '\n';
 
-    this.nox += '0ScRiPtSePaRaToR' + String(this.width) + '|' + String(this.height) + '|MULTI:1:0:' + Math.round(positions[position].width * this.width) + ':' + Math.round(positions[position].height * this.height) + 'ScRiPtSePaRaToR' + String(Math.round(noxMilliseconds + noxMilliseconds * frame)) + '\n';
+    this.nox += '0ScRiPtSePaRaToR' + String(this.width) + '|' + String(this.height) + '|MULTI:1:0:' + width + ':' + height + 'ScRiPtSePaRaToR' + String(Math.round(noxMilliseconds + noxMilliseconds * frame)) + '\n';
     this.nox += '0ScRiPtSePaRaToR' + String(this.width) + '|' + String(this.height) + '|MULTI:0:6' + 'ScRiPtSePaRaToR' + String(Math.round(noxMilliseconds + noxMilliseconds * frame + noxSeparator)) + '\n';
     this.nox += '0ScRiPtSePaRaToR' + String(this.width) + '|' + String(this.height) + '|MULTI:0:6' + 'ScRiPtSePaRaToR' + String(Math.round(noxMilliseconds + noxMilliseconds * frame + noxSeparator)) + '\n';
     this.nox += '0ScRiPtSePaRaToR' + String(this.width) + '|' + String(this.height) + '|MULTI:0:1' + 'ScRiPtSePaRaToR' + String(Math.round(noxMilliseconds + noxMilliseconds * frame + noxSeparator)) + '\n';
