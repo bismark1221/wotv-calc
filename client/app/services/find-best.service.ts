03393c871147063e6f856c784906817b2b4bd715
Yoann GALLAND <bahamut1221@gmail.com>
2017-10-21 16:16:55 +0200

    Update chain spark hitter calculation for find best

--- client/app/services/find-best.service.ts
+++ client/app/services/find-best.service.ts
@@ -314,7 +314,7 @@ export class FindBestService {
         if (unit.frames.length > nbCombo[index] &&
           (index === 0
             || unit.frames[nbCombo[index]].frame < minFrame
-            || (unit.frames[nbCombo[index]].frame === minFrame && lastHitter !== index))
+            || (unit.frames[nbCombo[index]].frame === minFrame && minIndex > index))
         ) {
           minFrame = unit.frames[nbCombo[index]].frame;
           minIndex = index;
