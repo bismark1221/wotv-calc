e3fe9b387667d70a35e1aece8c8d1a03370540d8
Yoann Galland <ygaland.prestataire@voyages-sncf.com>
2017-11-30 10:57:56 +0100

    Add a panel on the home page to show last updated units

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { UnitService } from '../services/unit.service'
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,34 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 export class HomeComponent {
   markdown = '';
+  updated = [81,80];
+  units = [];
 
-  constructor(private translateService: TranslateService) {
+  constructor(
+    private translateService: TranslateService,
+    private unitService: UnitService
+  ) {
     this.getTranslation();
 
     this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
       this.getTranslation();
+      this.getUnits();
     });
   }
 
   private getTranslation() {
     this.translateService.get('home.markdown').subscribe((res: string) => {
       this.markdown = res;
+    });
+  }
+
+  private getUnits() {
+    this.units = [];
+    this.unitService.getUnits().then(units => {
+      this.updated.forEach(id => {
+        let unit = this.unitService.getUnit(id);
+        this.units.push(unit.getName(this.translateService));
+      });
     });
   }
 }
