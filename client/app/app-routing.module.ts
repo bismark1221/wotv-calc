56314d768d83838bbbac768c11c7c82685281c0e
Yoann GALLAND <bahamut1221@gmail.com>
2018-07-04 14:06:57 +0200

    Missing clean

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ChainingComponent } from './chaining/chaining.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MyUnitsComponent } from './my-units/my-units.component';
 import { ContactComponent } from './contact/contact.component';
 import { LegalComponent } from './legal/legal.component';
 import { HowToComponent } from './how-to/how-to.component';
