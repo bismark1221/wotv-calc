b66b728796a77a3fddb15edb0c94e09cae1972c3
Yoann GALLAND <bahamut1221@gmail.com>
2020-04-18 18:21:48 +0200

    add passive

--- client/app/services/json.service.ts
+++ client/app/services/json.service.ts
@@ -59,6 +59,15 @@ export class JsonService {
     "spirit"
   ];
 
+  buffTypes = {
+    1: "HP",
+    21: "ATK",
+    22: "DEF",
+    25: "DEX",
+    26: "AGI",
+    27: "LCK"
+  }
+
   constructor(private http: HttpClient, private unitService: UnitService) {}
 
   private jsonUnits() {
@@ -107,14 +116,14 @@ export class JsonService {
       this.jsonBuffs(),
       this.jsonBuffNames(),
     ]).then(responses => {
-      this.units = responses[0].items;
-      this.names.unit = responses[1].infos;
-      this.names.job = responses[2].infos;
-      this.boards = responses[3];
-      this.skills = responses[4];
-      this.names.skills = responses[5].infos;
-      this.buffs = responses[6];
-      this.buffs.skill = responses[7].infos;
+      this.units = this.formatJson(responses[0].items);
+      this.names.unit = this.formatNames(responses[1].infos);
+      this.names.job = this.formatNames(responses[2].infos);
+      this.boards = this.formatJson(responses[3].items);
+      this.skills = this.formatJson(responses[4].items);
+      this.names.skill = this.formatNames(responses[5].infos);
+      this.buffs = this.formatJson(responses[6].items);
+      this.names.buff = this.formatNames(responses[7].infos);
 
 
       console.log(this.units)
@@ -128,9 +137,27 @@ export class JsonService {
     });
   }
 
+  private formatJson(data) {
+    let formatted = {};
+    data.forEach(item => {
+      formatted[item.iname] = item;
+    })
+
+    return formatted;
+  }
+
+  private formatNames(data) {
+    let formatted = {};
+    data.forEach(item => {
+      formatted[item.key] = item.value;
+    })
+
+    return formatted;
+  }
+
   private formatJsons() {
-    this.units.forEach(unit => {
-      let id = this.addUnit(unit);
+    Object.keys(this.units).forEach(unitId => {
+      let id = this.addUnit(this.units[unitId]);
 
       // if (id !== null && this.units[unitId].skills) {
       //   this.units[unitId].skills.forEach((ability, index) => {
@@ -158,12 +185,15 @@ export class JsonService {
         rarity: this.rarity[unit.rare],
         jobs: [{}, {}, {}],
         skills: [],
-        buffs: [],
-        type: unit.type
+        buffs: []
       };
 
       this.getUnitNames(this.wotvChainUnits[id]);
       this.getJobNames(this.wotvChainUnits[id], unit.jobsets);
+
+      if (id == 0) {
+        this.getSkillsAndBuffs(this.wotvChainUnits[id]);
+      }
 
       this.isCollapsed.push(true);
 
@@ -174,48 +204,142 @@ export class JsonService {
   }
 
   private getUnitNames(unit) {
-    let name = null;
-    let count = 0;
-
-    while (!unit.names.en) {
-      if (this.names.unit[count] && this.names.unit[count].key == unit.dataId) {
-        unit.names.en = this.names.unit[count].value
-      } else if (count >= this.names.unit.length) {
-        unit.names.en = unit.dataId;
-      } else {
-        count += 1
-      }
+    if (this.names.unit[unit.dataId]) {
+      unit.names.en = this.names.unit[unit.dataId]
+    } else {
+      unit.names.en = unit.dataId;
     }
   }
 
   private getJobNames(unit, jobs) {
-    let name = null;
-    let count = 0;
-
-    while (unit.jobs[0].en == null || unit.jobs[1].en == null || unit.jobs[2].en == null) {
-      if (this.names.job[count]) {
-        switch (this.names.job[count].key) {
-          case jobs[0]:
-              unit.jobs[0].en = this.names.job[count].value
-            break;
-          case jobs[1]:
-              unit.jobs[1].en = this.names.job[count].value
-            break;
-          case jobs[2]:
-              unit.jobs[2].en = this.names.job[count].value
-            break;
-        }
-        count += 1
-      } else if (count >= this.names.job.length) {
-        if (!unit.jobs[0].en)
-          unit.jobs[0].en = jobs[0];
-        if (!unit.jobs[1].en)
-          unit.jobs[1].en = jobs[1];
-        if (!unit.jobs[2].en)
-          unit.jobs[2].en = jobs[2];
+    for (let i = 0; i < 3; i++) {
+      if (this.names.job[jobs[i]]) {
+        unit.jobs[i].en = this.names.job[jobs[i]]
       } else {
-        count += 1
+        unit.jobs[i].en = jobs[i];
       }
     }
+  }
+
+  private getSkillsAndBuffs(unit) {
+    console.log("=====")
+    // console.log(this.boards)
+    // console.log(this.skills)
+    // console.log(this.buffs)
+
+
+    if (this.boards[unit.dataId]) {
+      this.boards[unit.dataId].panels.forEach(item => {
+        // console.log(item)
+
+        if (item.value.split("_")[0] === "BUFF") {
+          // console.log("buff")
+          this.addPassiveBuff(unit, item)
+        } else {
+          this.addSkill(unit, item)
+          // console.log("skill")
+        }
+      });
+    }
+
+    console.log("=====")
+  }
+
+  private addPassiveBuff(unit, panelBuff) {
+    // console.log(panelBuff)
+    // console.log(this.buffs[panelBuff.value])
+
+    let buff = {
+      unlockStar: panelBuff.unlock_value + 1,
+      unlockJob: panelBuff.get_job,
+      jobLevel: panelBuff.need_level,
+      jp: panelBuff.jp,
+      effects: []
+    };
+
+    let finished = false;
+    let i = 1;
+    while (!finished) {
+      if (this.buffs[panelBuff.value]["type" + i]) {
+        buff.effects.push({
+          type: this.buffTypes[this.buffs[panelBuff.value]["type" + i]],
+          value: this.buffs[panelBuff.value]["val" + i],
+          percent: this.buffs[panelBuff.value]["calc1"] == 2 ? true : undefined
+        });
+
+
+        // if (this.buffs[panelBuff.value]["calc1"] == 2) {
+        //   console.log(panelBuff.value)
+        //   console.log(this.buffs[panelBuff.value]["type" + i])
+        // }
+
+
+        i++;
+      } else {
+        finished = true;
+      }
+    }
+
+    unit.buffs.push(buff);
+  }
+
+  private addSkill(unit, panelSkill) {
+    console.log(panelSkill)
+    console.log(this.skills[panelSkill.value])
+
+    let skill = {
+      unlockStar: panelSkill.unlock_value + 1,
+      unlockJob: panelSkill.get_job,
+      jobLevel: panelSkill.need_level,
+      name: this.names.skill[panelSkill.value],
+      effects: [],
+      iname: panelSkill.value
+    };
+
+    if (typeof(this.skills[panelSkill.value].cost_type) == "number") {
+      skill.cost = {
+        type: this.skills[panelSkill.value].cost_type == 0 ? "AP" : "TP",
+        value: this.skills[panelSkill.value].cost_type == 0 ? this.skills[panelSkill.value].cost_ap : this.skills[panelSkill.value].cost_mp
+      }
+      skill.type = "active"
+      skill.range = {
+        h: this.skills[panelSkill.value].range_h,
+        l: this.skills[panelSkill.value].range_l,
+        mh: this.skills[panelSkill.value].range_mh,
+        s: this.skills[panelSkill.value].range_s
+      }
+
+
+
+
+
+
+
+
+    } else if (this.skills[panelSkill.value].s_buffs) {
+      skill.type = "passive"
+      this.skills[panelSkill.value].s_buffs.forEach(buff => {
+        let finished = false;
+        let i = 1;
+        while (!finished) {
+          if (this.buffs[buff]["type" + i]) {
+            skill.effects.push({
+              type: this.buffTypes[this.buffs[buff]["type" + i]],
+              minValue: this.buffs[buff]["val" + i],
+              maxValue: this.buffs[buff]["val" + i + "1"],
+              percent: this.buffs[buff]["calc1"] == 2 ? true : undefined
+            });
+            i++;
+          } else {
+            finished = true;
+          }
+        }
+      })
+    } else {
+      console.log("counter")
+      skill.type = "counter"
+    }
+
+    unit.skills.push(skill);
   }
 }
