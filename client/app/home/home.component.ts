5d39ecafdbfef7d1cac7dec2a770b6af7c496afb
bahamut1221 <bahamut1221@gmail.com>
2020-06-06 19:56:58 +0200

    Add date to home page

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { EsperService } from '../services/esper.service'
 import { CardService } from '../services/card.service'
 import { NavService } from '../services/nav.service'
 import { NameService } from '../services/name.service'
+import { JobService } from '../services/job.service'
 
 @Component({
   selector: 'app-home',
@@ -19,40 +20,58 @@ export class HomeComponent {
 
   updated = {
     GL: [
-      [
-        {type: "unit", dataId: "UN_LW_P_LUCA"},
-        {type: "unit", dataId: "UN_LW_P_ADLD"},
-        {type: "esper", dataId: "UN_LW_S_TSLP"},
-        {type: "card", dataId: "VC_LW_TSLP"},
-        {type: "card", dataId: "VC_LW_LILS"}
-      ],
-      [
-        {type: "unit", dataId: "UN_LW_P_MRND"},
-        {type: "unit", dataId: "UN_LW_P_DRND"},
-        {type: "esper", dataId: "UN_LW_S_LAMA_01"},
-        {type: "card", dataId: "VC_LW_LAMA_01"},
-        {type: "card", dataId: "VC_LW_GUARD"}
-      ],
-      [
-        {type: "esper", dataId: "UN_LW_S_ODIN"},
-        {type: "card", dataId: "VC_LW_ODIN"},
-        {type: "card", dataId: "VC_LW_NEWYEAR"}
-      ]
+      {
+        date: "03/06",
+        items: [
+          {type: "unit", dataId: "UN_LW_P_LUCA"},
+          {type: "unit", dataId: "UN_LW_P_ADLD"},
+          {type: "esper", dataId: "UN_LW_S_TSLP"},
+          {type: "card", dataId: "VC_LW_TSLP"},
+          {type: "card", dataId: "VC_LW_LILS"}
+        ]
+      },
+      {
+        date: "20/05",
+        items: [
+          {type: "unit", dataId: "UN_LW_P_MRND"},
+          {type: "unit", dataId: "UN_LW_P_DRND"},
+          {type: "esper", dataId: "UN_LW_S_LAMA_01"},
+          {type: "card", dataId: "VC_LW_LAMA_01"},
+          {type: "card", dataId: "VC_LW_GUARD"}
+        ]
+      },
+      {
+        date: "13/05",
+        items: [
+          {type: "esper", dataId: "UN_LW_S_ODIN"},
+          {type: "card", dataId: "VC_LW_ODIN"},
+          {type: "card", dataId: "VC_LW_NEWYEAR"}
+        ]
+      }
     ],
     JP: [
-      [
-        {type: "unit", dataId: "UN_LW_P_GRSR"},
-        {type: "unit", dataId: "UN_LW_P_LSWL"},
-        {type: "unit", dataId: "UN_LW_P_RRIR"}
-      ],
-      [
-        {type: "esper", dataId: "UN_LW_S_FNRR"},
-        {type: "card", dataId: "VC_LW_FNRR"},
-        {type: "card", dataId: "VC_LW_ORDR"}
-      ],
-      [
-        {type: "unit", dataId: "UN_LW_P_STRN_01"}
-      ]
+      {
+        date: "31/05",
+        items: [
+          {type: "unit", dataId: "UN_LW_P_GRSR"},
+          {type: "unit", dataId: "UN_LW_P_LSWL"},
+          {type: "unit", dataId: "UN_LW_P_RRIR"}
+        ]
+      },
+      {
+        date: "22/05",
+        items: [
+          {type: "esper", dataId: "UN_LW_S_FNRR"},
+          {type: "card", dataId: "VC_LW_FNRR"},
+          {type: "card", dataId: "VC_LW_ORDR"}
+        ]
+      },
+      {
+        date: "18/05",
+        items: [
+          {type: "unit", dataId: "UN_LW_P_STRN_01"}
+        ]
+      }
     ]
   };
 
@@ -64,7 +83,8 @@ export class HomeComponent {
     private esperService: EsperService,
     private cardService: CardService,
     private navService: NavService,
-    private nameService: NameService
+    private nameService: NameService,
+    private jobService: JobService,
   ) {
     this.getTranslation();
     this.getUpdate();
@@ -86,21 +106,18 @@ export class HomeComponent {
   }
 
   private getUpdate() {
-    let lang = this.translateService.currentLang
     let version = this.navService.getVersion()
 
     this.updated[version].forEach((update, updateIndex) => {
-      this.updatedFormatted[updateIndex] = []
+      this.updatedFormatted[updateIndex] = {
+        date: update.date,
+        items: []
+      }
       let tableIndex = -1;
 
-      update.forEach((item, itemIndex) => {
-        if (itemIndex % 4 === 0) {
-          tableIndex++;
-          this.updatedFormatted[updateIndex][tableIndex] = [];
-        }
-
+      update.items.forEach((item, itemIndex) => {
         let dataItem = this[item.type + "Service"]["get" + item.type[0].toUpperCase() + item.type.slice(1)](item.dataId);
-        this.updatedFormatted[updateIndex][tableIndex].push({
+        this.updatedFormatted[updateIndex].items.push({
           type: item.type,
           slug: dataItem.slug,
           name: this.nameService.getName(dataItem),
@@ -108,6 +125,14 @@ export class HomeComponent {
           element: dataItem.element,
           rarity: item.type == "esper" ? this.esperService.findRarity(dataItem) : dataItem.rarity
         });
+
+        if (item.type == "unit") {
+          this.updatedFormatted[updateIndex].items[itemIndex].jobs = []
+          dataItem.jobs.forEach(jobId => {
+            let job = this.jobService.getJob(jobId)
+            this.updatedFormatted[updateIndex].items[itemIndex].jobs.push(job)
+          })
+        }
       });
     });
   }
