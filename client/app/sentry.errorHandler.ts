df806345b21773b41cae14f9fb37a0bf43b11fa5
Yoann GALLAND <bahamut1221@gmail.com>
2020-07-20 09:46:21 +0200

    Set sentry release

--- client/app/sentry.errorHandler.ts
+++ client/app/sentry.errorHandler.ts
@@ -13,6 +13,7 @@ Sentry.init({
   // TryCatch has to be configured to disable XMLHttpRequest wrapping, as we are going to handle
   // http module exceptions manually in Angular's ErrorHandler and we don't want it to capture the same error twice.
   // Please note that TryCatch configuration requires at least @sentry/browser v5.16.0.
+  release: "prod",
   integrations: [new Sentry.Integrations.TryCatch({
     XMLHttpRequest: false,
   })],
