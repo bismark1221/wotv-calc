121d9e0b0f72c00dc4d40530d542cbcf4b82029d
bahamut1221 <bahamut1221@gmail.com>
2020-08-07 17:25:24 +0200

    Add new files

---
+++ client/app/entities/item.ts
@@ -0,0 +1,26 @@
+export class Item {
+  dataId?;
+  names = {
+    en: 'New Item'
+  };
+  name = 'New Item'
+  recipe = false
+  //slug = 'new-item'
+
+  constructFromJson(item: Item): void {
+    this.dataId = item.dataId;
+    this.names = item.names;
+    this.recipe = item.recipe;
+    //this.slug = raid.slug;
+  }
+
+  getName(translateService): string {
+    if (!this.names[translateService.currentLang]) {
+      this.name = this.names[translateService.getDefaultLang()];
+    } else {
+      this.name = this.names[translateService.currentLang];
+    }
+
+    return this.name;
+  }
+}
