874167321ae7fd7b00b43453a08b6510aee98271
Yoann GALLAND <bahamut1221@gmail.com>
2020-09-14 13:51:15 +0200

    Design login

--- client/app/nav/nav.component.ts
+++ client/app/nav/nav.component.ts
@@ -140,7 +140,7 @@ export class NavComponent {
   }
 
   openLoginModal() {
-    const modalRef = this.modalService.open(LoginComponent, { windowClass: 'builder-modal' });
+    const modalRef = this.modalService.open(LoginComponent, { windowClass: 'login-modal' });
 
     modalRef.result.then((user) => {}, (reason) => {});
   }
