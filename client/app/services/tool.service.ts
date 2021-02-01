9630cd2a36f09f78ae11fb528c9bf96fd4fac1e1
bahamut1221 <bahamut1221@gmail.com>
2021-02-01 11:58:37 +0100

    Rework sort on name

--- client/app/services/tool.service.ts
+++ client/app/services/tool.service.ts
@@ -5,68 +5,27 @@ import { TranslateService } from '@ngx-translate/core';
 @Injectable()
 export class ToolService {
 
-  private re = /(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g;
   private sre = /^\s+|\s+$/g;
-  private snre = /\s+/g;
-  private dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/;
-  private hre = /^0x[0-9a-f]+$/i;
-  private ore = /^0/;
-  private oFxNcL: any;
-  private oFyNcL: any;
 
   constructor(
     private translateService: TranslateService
   ) {}
 
   private i(s: any) {
-      return (('' + s).toLowerCase() || '' + s).replace(this.sre, '');
-  }
-
-  private normChunk(s: any, l: any) {
-      return (!s.match(this.ore) || l === 1) && parseFloat(s) || s.replace(this.snre, ' ').replace(this.sre, '') || 0;
+    return (('' + s).toLowerCase() || '' + s).replace(this.sre, '');
   }
 
   sortByName(items, order = 'asc') {
     items.sort((a: any, b: any) => {
-      const x = this.i(a.name ? a.name : a.getName(this.translateService));
-      const y = this.i(b.name ? b.name : b.getName(this.translateService));
+      const x = this.i(a.name && a.name !== "new unit" ? a.name : a.getName(this.translateService));
+      const y = this.i(b.name && b.name !== "new unit" ? b.name : b.getName(this.translateService));
 
-      const xN = x.replace(this.re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0');
-      const yN = y.replace(this.re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0');
-
-      const xD = parseInt((<any>x).match(this.hre), 16) || (xN.length !== 1 && Date.parse(x));
-      const yD = parseInt((<any>y).match(this.hre), 16) || xD && y.match(this.dre) && Date.parse(y) || null;
-
-      if (yD) {
-        if (xD < yD) {
-          return order === 'asc' ? -1 : 1;
-        } else if (xD > yD) {
-          return order === 'asc' ? 1 : -1;
-        }
+      if (order === 'asc') {
+        return x.localeCompare(y, 'ja');
+      } else {
+        return y.localeCompare(x, 'ja');
       }
 
-      for (let cLoc = 0, xNl = xN.length, yNl = yN.length, numS = Math.max(xNl, yNl); cLoc < numS; cLoc++) {
-        this.oFxNcL = this.normChunk(xN[cLoc] || '', xNl);
-        this.oFyNcL = this.normChunk(yN[cLoc] || '', yNl);
-        if (isNaN(this.oFxNcL) !== isNaN(this.oFyNcL)) {
-          if (isNaN(this.oFxNcL)) {
-            return order === 'asc' ? 1 : -1;
-          } else {
-            return order === 'asc' ? -1 : 1;
-          }
-        }
-
-        if (/[^\x00-\x80]/.test(this.oFxNcL + this.oFyNcL) && this.oFxNcL.localeCompare) {
-          const comp = this.oFxNcL.localeCompare(this.oFyNcL);
-          return comp / Math.abs(comp);
-        }
-
-        if (this.oFxNcL < this.oFyNcL) {
-          return order === 'asc' ? -1 : 1;
-        } else if (this.oFxNcL > this.oFyNcL) {
-          return order === 'asc' ? 1 : -1;
-        }
-      }
     });
 
     return items;
@@ -87,28 +46,7 @@ export class ToolService {
         const x = this.i(a.getName(this.translateService));
         const y = this.i(b.getName(this.translateService));
 
-        const xN = x.replace(this.re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0');
-        const yN = y.replace(this.re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0');
-
-        const xD = parseInt((<any>x).match(this.hre), 16) || (xN.length !== 1 && Date.parse(x));
-        const yD = parseInt((<any>y).match(this.hre), 16) || xD && y.match(this.dre) && Date.parse(y) || null;
-
-        if (yD) {
-            if (xD < yD) { return -1; } else if (xD > yD) { return 1; }
-        }
-
-        for (let cLoc = 0, xNl = xN.length, yNl = yN.length, numS = Math.max(xNl, yNl); cLoc < numS; cLoc++) {
-            this.oFxNcL = this.normChunk(xN[cLoc] || '', xNl);
-            this.oFyNcL = this.normChunk(yN[cLoc] || '', yNl);
-            if (isNaN(this.oFxNcL) !== isNaN(this.oFyNcL)) {
-                return isNaN(this.oFxNcL) ? 1 : -1;
-            }
-            if (/[^\x00-\x80]/.test(this.oFxNcL + this.oFyNcL) && this.oFxNcL.localeCompare) {
-                const comp = this.oFxNcL.localeCompare(this.oFyNcL);
-                return comp / Math.abs(comp);
-            }
-            if (this.oFxNcL < this.oFyNcL) { return -1; } else if (this.oFxNcL > this.oFyNcL) { return 1; }
-        }
+        return x.localeCompare(y, 'ja');
       }
     });
 
