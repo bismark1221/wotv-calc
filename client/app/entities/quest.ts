5d688d54fa9269a203e151631b3a33444f0c4c25
bahamut1221 <bahamut1221@gmail.com>
2021-04-13 10:11:36 +0200

    Remove duplicate variables

--- client/app/entities/quest.ts
+++ client/app/entities/quest.ts
@@ -13,33 +13,36 @@ export class Quest {
   exp = 0;
   nrg = 0;
   jp = 0;
-  enemies = 0;
   gils = 0;
-  chests = [];
-  missions = [];
+
+  enemies = [];
   chests = [];
   allies = [];
   objects = [];
   switchs = [];
+
   missions = [];
   grid = [];
 
 
   constructFromJson(quest, translateService) {
     this.dataId = quest.dataId;
+    this.names = quest.names;
     this.slug = quest.slug;
     this.type = quest.type;
+
     this.items = quest.items;
     this.exp = quest.exp;
     this.nrg = quest.nrg;
     this.jp = quest.jp;
     this.gils = quest.gils;
+
     this.enemies = quest.enemies;
     this.chests = quest.chests;
     this.allies = quest.allies;
     this.objects = quest.objects;
     this.switchs = quest.switchs;
-    this.names = quest.names;
+
     this.missions = quest.missions;
     this.grid = quest.grid;
   }
