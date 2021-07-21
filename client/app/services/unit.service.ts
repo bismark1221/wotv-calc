4adb4d0d08136cc84c6849e39d38cc9562de9166
Bismark <bahamut1221@gmail.com>
2021-07-21 23:35:47 +0200

    Fix unit page

--- client/app/services/unit.service.ts
+++ client/app/services/unit.service.ts
@@ -294,8 +294,10 @@ export class UnitService {
 
   async formatSkills(unit, forcedVersion = null) {
     unit.formattedUnlockedSkills = [];
-    for (const skillId of unit.unlockedSkills) {
-      unit.formattedUnlockedSkills.push(await this.skillService.getSkill(skillId, forcedVersion));
+    if (unit.unlockedSkills) {
+      for (const skillId of unit.unlockedSkills) {
+        unit.formattedUnlockedSkills.push(await this.skillService.getSkill(skillId, forcedVersion));
+      }
     }
 
     for (const nodeId of Object.keys(unit.board.nodes)) {
