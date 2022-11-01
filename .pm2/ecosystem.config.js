2b2b5141603b16183c86c780683c2f20a82a1c38
Bismark <bahamut1221@gmail.com>
2022-11-01 20:01:00 +0100

    Update deploy script for test env

--- .pm2/ecosystem.config.js
+++ .pm2/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: "wotv-calc-front",
-    script    : "/data/www/wotv-calc/dist/wotv-calc/server/main.js",
+    name: "wotv-calc-front-test",
+    script    : "/data/www/design-wotv/dist/wotv-calc/server/main.js",
     instances : "max",
     exec_mode : "cluster",
     log_date_format: "DD-MM HH:mm:ss.SSS"
