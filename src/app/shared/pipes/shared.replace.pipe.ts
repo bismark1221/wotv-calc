9d3761c8acdcfc75388a072fc5c41f167868ddd8
Bismark <bahamut1221@gmail.com>
2022-11-04 15:38:36 +0100

    #59 - Replace _ by space on effect for team builder

---
+++ src/app/shared/pipes/shared.replace.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'replace' })
+export class SharedReplacePipe implements PipeTransform {
+  transform(value: string, strToReplace: string, replacementStr: string): string {
+
+    if (!value || !strToReplace || !replacementStr) {
+      return value;
+    }
+
+    return value.replace(new RegExp(this.escapeStr(strToReplace), 'g'), replacementStr);
+  }
+
+  escapeStr(str) {
+    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  }
+}
