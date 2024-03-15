eb18888e025a13893ebcf9cd166862876cff81eb
Bismark <bahamut1221@gmail.com>
2024-03-15 10:32:47 +0100

    JP & GL update

--- src/app/universal-relative.interceptor.ts
+++ src/app/universal-relative.interceptor.ts
@@ -18,14 +18,12 @@ export class UniversalRelativeInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     if (this.request && !isAbsoluteURL(req.url)) {
       let host = this.request.get('host');
-      let protocol = this.request.protocol;
 
       if (host.split(':')[0] === 'localhost' && req.url.split('/')[1] === 'api') {
-        host = 'localhost:4200';
-        protocol = 'http';
+        host = 'localhost:4201';
       }
 
-      const protocolHost = `${protocol}://${host}`;
+      const protocolHost = `${this.request.protocol}://${host}`;
       const pathSeparator = !req.url.startsWith('/') ? '/' : '';
       const url = protocolHost + pathSeparator + req.url;
       const serverRequest = req.clone({ url });
